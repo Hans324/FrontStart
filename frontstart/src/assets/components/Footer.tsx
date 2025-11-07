0017a454d958f51891b1963a613ad444dbc97518
Hans Siemon <hanssiemon@hanss-air.home>
2025-11-07 19:00:23 +0100

    stylisation du Footer desktop

--- frontstart/src/assets/components/Footer.tsx
+++ frontstart/src/assets/components/Footer.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import SiteLogo from '../images/SiteLogo.png'
+import Linkedin from '../images/Linkedin.png'
+import Twitter from '../images/Twitter.png'
+import Discord from '../images/Discord.png'
 import './Footer.css'
 
 
@@ -30,18 +33,21 @@ function Footer() {
 </section>
 </section>
 <section className='Partie-quatre'>
-<section className='Résaux Sociaux'>
+<section className='Réseaux-Sociaux'>
     <h3>Réseaux sociaux</h3>
-
+      <img src= {Linkedin}  alt ="Logo de Linkedin" className='Linkedin'/>
+      <img  src= {Twitter}  alt ="Logo de Twitter" className='Twitter'/>
+      <img src= {Discord}  alt ="Logo de Discord" className='Discord'/>
 </section>
 </section>
 <section>
+<section className='Partie-cinq'>
 <section className='Mentions'> 
 <h3>Mentions légales</h3>
 <p>Mentions légales</p>
 <p>Politique de confidentialté </p>
 <p>Conditions d'utilisation</p>
-
+</section>
 </section>
 </section>
 </section>
