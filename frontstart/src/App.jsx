a3d136062b64aa233598469a4c679506edf0ec67
Hans Siemon <hanssiemon@hanss-air.home>
2025-11-13 12:24:09 +0100

    création des routes

--- frontstart/src/App.jsx
+++ frontstart/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowerRouter, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home.tsx";
 import Footer from './assets/components/Footer.tsx';
 import Navbar from './pages/Navbar.tsx';
+import Langages from './pages/Langages.tsx';
+import HTML from './pages/HTML.tsx'
+import CSS from './pages/CSS.tsx'
+import JavaScript from './pages/JavaScript.tsx'
 
 
 
@@ -13,12 +14,22 @@ function App() {
 
   return (
     <>
+    <BrowerRouter>
     <Navbar/>
-     <Home/>
-
+     <Routes>
+     <Route path="/" element={<Home/>} />
+     <Route path="/Langages" element={<Langages/>} />
+     <Route path="/Ecoles" element={<Ecoles/>} />
+     <Route path="/Tools" element={<Tools/>} />
+     <Route path="/Contact" element={<Contact/>} />
+     <Route path="/HTML" element={<HTML/>} />
+     <Route path="/CSS" element={<CSS/>} />
+     <Route path="JavaScript" element={<JavaScript/>} />
+     </Routes>
      <Footer/>
+     </BrowerRouter>
     </>
   )
 }
 
-export default App
+export default App;
--- frontstart/src/pages/CSS.tsx
+++ frontstart/src/pages/CSS.tsx
@@ -1,3 +1,45 @@
 import React from "react";
-
 import CSSLogo from '../assets/images/CSS.png';
+import './CSS.css'
+
+
+
+
+
+function CSS () {
+
+
+return(
+
+ <section>
+<section>
+<h1>Cours de CSS</h1>
+<img src ={CSSLogo} alt= "Logo de CSS" className="Image CSS" />
+
+</section>
+<h3>CSS: Mettre en forme le contenu</h3>
+<p>CSS (pour Cascading Style Sheets en anglais, qu'on peut traduire en « feuilles de style en cascade ») est le code utilisé pour mettre en forme une page web. 
+ Cet article vous guide à travers les bases de CSS : comment cela fonctionne et comment améliorer l'apparence et la présentation de la structure de contenu que vous avez créée dans l'article précédent.</p>
+
+<section>
+<h3>Qu'est-ce que CSS?</h3>
+<p>
+
+Comme le HTML, CSS n'est pas un langage de programmation. Ce n'est pas non plus un langage de balisage. CSS est un langage de feuille de style. 
+CSS sert à mettre en forme les éléments HTML : vous sélectionnez les éléments à mettre en forme et vous définissez des valeurs pour leurs propriétés de style, ce qui détermine leur apparence.</p>
+</section>
+<section>
+<h3> Appliquer du CSS à votre HTML</h3>
+<p>Lorsque vous utilisez CSS, la première chose à vérifier est que votre CSS est bien appliqué à votre HTML. 
+    Dans cette section, nous allons ajouter une feuille de style CSS à votre dossier mon-premier-site et l'appliquer à votre page.</p>
+
+</section>
+ </section>
+
+)
+
+
+}
+
+
+export default CSS;
