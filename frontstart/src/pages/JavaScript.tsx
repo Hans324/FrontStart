708192c974ff794e9010621a6525a86aab7797a0
Hans Siemon <hanssiemon@hanss-air.home>
2025-12-01 13:09:13 +0100

    creation de la structure et stylisation de JavaScript.tsx et creation du fichier JavaScript.css

--- frontstart/src/pages/JavaScript.tsx
+++ frontstart/src/pages/JavaScript.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-
-
 import JavaScriptLogo from '../assets/images/JavaScript.png';
-
+import './JavaScript.css'
 
 
 
@@ -10,7 +8,18 @@ function JavaScript() {
 
 return (
 <section>
+    <section>
+<h1 className="title-Cours">Cours de JavaScript</h1>
+<img src ={JavaScriptLogo} alt= "Logo de JavaScript" className="Image-JavaScript" />
+</section>
+<section className=" section-1">
+<h2 className="titre-un">Une définition générale</h2>
 
+<p className="text-un">JavaScript est une langage de programmation qui permet d'implémenter des mécanismes complexes sur une page web.À chaque fois qu'un page web fait plus que simplement afficher du contenu statique— afficher du contenu mis à jour à des temps déterminés, des cartes interactives, des animations 2D/3D, des menus vidéo défilants, ou autre, JavaScript a de bonnes chances d'être impliqué. 
+    C'est la troisième couche des technologies standards du web,les Deux premières (HTML et CSS).</p>
+
+
+</section>
 </section>
 
 
